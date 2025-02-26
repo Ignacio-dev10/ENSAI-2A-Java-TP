b08f352411d25df62626bc004027c6bb7bf1320a
Ignacio-dev10 <ignacio.espinosamart@gmail.com>
2025-02-26 09:46:32 +0000

    changementPassword

--- tp1/src/Password.java
+++ tp1/src/Password.java
@@ -40,7 +40,16 @@ public class Password {
      */
     public static String bruteForce6Digit(String targetHash) {
 
-        // Code here
+        for (int i = 0; i < 1000000; i++) {
+            String test = String.format("%06d", i);
+            String mdp = hashPassword(test);
+            if (test != mdp) {
+                continue;
+            } else {
+                System.out.println("Votre mot de passe est: " + i);
+                break;
+            }
+        }
 
         return null;
     }
